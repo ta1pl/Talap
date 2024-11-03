b93063b30365e98a5c7b81e976c2ea31b80bce6c
taipl <231741@astanait.edu.kz>
2024-11-03 18:10:10 +0500

    2

--- src/Person.java
+++ src/Person.java
@@ -7,36 +7,28 @@ public abstract class Person implements Payable, Comparable<Person>{
     public Person(){
         this.id = id_gen++;
     }
-
     public Person(String name, String surname){
         this();
         setName(name);
         setSurname(surname);
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getName() {
         return name;
     }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public int getId() {
         return id;
     }
-
     public String ToString(){
         return this.id + ". " + this.name + " " + this.surname;
     }
-
     public abstract String GetPosition();
 }
